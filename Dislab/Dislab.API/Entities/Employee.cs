2c5096a3c1aaa9b0fc4d42b3e7c69249003ef65f
jahids-web <jahiduddin922@gmail.com>
2023-12-05 23:51:55 +0600

    Entitie Model Class

---
+++ Dislab/Dislab.API/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Dislab.API.Entities
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+    }
+  
+
+
+
+}
