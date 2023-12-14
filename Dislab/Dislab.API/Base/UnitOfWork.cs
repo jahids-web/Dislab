f93ed3eb9ea614aeaa8403a564a0c5151aadac19
jahids-web <jahiduddin922@gmail.com>
2023-12-14 22:54:12 +0600

    Create a New IUnitOfWork Class

---
+++ Dislab/Dislab.API/Base/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Dislab.API.Base
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public UnitOfWork(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public IEmployeeRepository EmployeeRepository { get; set ; }
+    }
+}
