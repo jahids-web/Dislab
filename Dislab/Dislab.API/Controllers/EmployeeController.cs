e66505125af15dbffce30040ddfc91d62111fa63
jahids-web <jahiduddin922@gmail.com>
2023-12-14 22:56:48 +0600

    Create a New EmployeeController

---
+++ Dislab/Dislab.API/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Dislab.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dislab.API.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeServices _employeeServices;
+
+        public EmployeeController(IEmployeeServices employeeServices) 
+        {
+            _employeeServices = employeeServices;
+        }
+
+
+    }
+}
