f2a95bc7881ed548a5031d42c0951d92eac3feae
jahids-web <jahiduddin922@gmail.com>
2023-12-14 23:11:46 +0600

    Create BaseModule Class

--- Dislab/Dislab.Base/BaseModule.cs
+++ Dislab/Dislab.Base/BaseModule.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace Dislab.Base
 {
     public class BaseModule
     {
+        public static void AllDependency(IServiceCollection services, IConfiguration configuration)
+        {
+      
+        }
     }
 }
