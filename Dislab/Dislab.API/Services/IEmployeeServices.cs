cfffbd3c7c68ade1295ca2a74caf374686b152e7
jahids-web <jahiduddin922@gmail.com>
2023-12-17 23:43:37 +0600

    Change Method Name

--- Dislab/Dislab.API/Services/IEmployeeServices.cs
+++ Dislab/Dislab.API/Services/IEmployeeServices.cs
@@ -4,7 +4,7 @@ namespace Dislab.API.Services
 {
     public interface IEmployeeServices
     {
-        public Employee Create(Employee employee);
+        public Employee Insert(Employee employee);
         public Employee Update(Employee employee);
         public Employee Delete(long id);
         public Employee GetEmployeeById(long id);
