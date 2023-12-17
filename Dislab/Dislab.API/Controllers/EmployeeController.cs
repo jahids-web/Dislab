80e1523d4da0fddfa453a721f022664e41086d07
jahids-web <jahiduddin922@gmail.com>
2023-12-17 23:42:13 +0600

    Change Method Name

--- Dislab/Dislab.API/Controllers/EmployeeController.cs
+++ Dislab/Dislab.API/Controllers/EmployeeController.cs
@@ -1,17 +1,27 @@
-﻿using Dislab.API.Services;
+﻿using Dislab.API.Entities;
+using Dislab.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dislab.API.Controllers
 {
-    public class EmployeeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeServices _employeeServices;
 
-        public EmployeeController(IEmployeeServices employeeServices) 
+        public EmployeeController(IEmployeeServices employeeServices)
         {
             _employeeServices = employeeServices;
         }
 
+        [HttpPost]
+        public Employee Insert(Employee request)
+        {
+            var result = _employeeServices.Insert(request);
+            return (result);
 
+        }
     }
 }
