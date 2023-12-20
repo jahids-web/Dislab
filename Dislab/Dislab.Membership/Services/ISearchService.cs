6900a7e3f35aa6077c53ce2b4e1aa823dcc68765
jahids-web <jahiduddin922@gmail.com>
2023-12-20 23:32:01 +0600

    Create ISearchService Interface

---
+++ Dislab/Dislab.Membership/Services/ISearchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Membership.Services
+{
+    public interface ISearchService
+    {
+    }
+
+    public class SearchService : ISearchService 
+    { 
+    
+    }
+}
