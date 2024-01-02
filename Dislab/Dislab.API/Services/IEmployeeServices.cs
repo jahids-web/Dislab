10a7d606e383a61ef36c7aa1ae3c4adffd203cfb
jahids-web <jahiduddin922@gmail.com>
2024-01-02 16:24:56 +0600

    Trying To Solve Error

--- Dislab/Dislab.API/Services/IEmployeeServices.cs
+++ Dislab/Dislab.API/Services/IEmployeeServices.cs
@@ -6,8 +6,8 @@ namespace Dislab.API.Services
     {
         public Employee Insert(Employee employee);
         public Employee Update(Employee employee);
-        public Employee Delete(long id);
-        public Employee GetEmployeeById(long id);
+        public long Delete(long id);
+        public long GetEmployeeById(long id);
         public IEnumerable<Employee> GetAll(Employee employee);
     }
 }
