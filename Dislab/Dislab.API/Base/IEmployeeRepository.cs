c59ed8d4fdf76de6b13b73ea436055d5ab206fe8
jahids-web <jahiduddin922@gmail.com>
2024-01-10 00:29:38 +0600

    Edit Some Code

--- Dislab/Dislab.API/Base/IEmployeeRepository.cs
+++ Dislab/Dislab.API/Base/IEmployeeRepository.cs
@@ -5,10 +5,10 @@ namespace Dislab.API.Base
     public interface IEmployeeRepository
     {
         public Employee Create(Employee employee);
-        public Employee Update(Employee employee);
+        public void Update(Employee employee);
         public long Delete(long id);
-        public long GetEmployeeById(long id);
+        public IEnumerable<Employee> GetEmployeeById(long id);
         public IEnumerable<Employee> GetAll(Employee employee);
-       
+
     }
 }
