cf7480b00245d3e4bea1914c92dc156eff23b1aa
jahids-web <jahiduddin922@gmail.com>
2024-01-13 00:13:22 +0600

    Create New Controller QuestionDetailsController

---
+++ Dislab/Dislab.Web/Areas/Admin/Controllers/QuestionDetailsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dislab.Web.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class QuestionDetailsController : Controller
+    {
+
+        private readonly ILogger<QuestionDetailsController> _logger;
+
+        public QuestionDetailsController(ILogger<QuestionDetailsController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
