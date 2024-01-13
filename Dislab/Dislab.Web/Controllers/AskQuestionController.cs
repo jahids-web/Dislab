f8f25a1338e75fe473bafef926a6054d80d71db6
jahids-web <jahiduddin922@gmail.com>
2024-01-13 11:52:51 +0600

    Add CRUD Method

--- Dislab/Dislab.Web/Controllers/AskQuestionController.cs
+++ Dislab/Dislab.Web/Controllers/AskQuestionController.cs
@@ -1,12 +1,71 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dislab.API.Entities;
+using Dislab.Base.Entities;
+using Dislab.Base.Repositories;
+using Dislab.Membership.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dislab.Web.Controllers
 {
     public class AskQuestionController : Controller
     {
+        private readonly IAskQuestionService _askQuestionService;
+
+        public AskQuestionController(IAskQuestionService askQuestionService)
+        {
+            _askQuestionService = askQuestionService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public AskQuestion Insert(AskQuestion question) 
+        {
+            var result = _askQuestionService.Insert(question);
+            return result;
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _askQuestionService.Delete(id);
+            return Ok(new { IsSuccess = true, Message = "Question Deleted Successfully." });
+        }
+
+        [HttpGet]
+        public IActionResult GetQuestionById(long id)
+        {
+            var question = _askQuestionService.GetByQuestionId(id);
+            return Ok(new
+            {
+                IsSuccess = true,
+                Message = "Question Successfully.",
+                Data = question
+            });
+        }
+
+        [HttpPut]
+        public void Update(AskQuestion askQuestion)
+        {
+            _askQuestionService.Update(askQuestion);
+        }
+
+        [HttpGet]
+        public IEnumerable<AskQuestion> GetAll()
+        {
+            var data = _askQuestionService.GetAll();
+            return data;
+        }
+
+
+
+
+
+
+
+
+
     }
 }
