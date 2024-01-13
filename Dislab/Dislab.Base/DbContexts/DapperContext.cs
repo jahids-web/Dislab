17330b8c0ba4300a09a4598d0c349fb766507854
jahids-web <jahiduddin922@gmail.com>
2024-01-13 11:50:19 +0600

    Create DapperContext

---
+++ Dislab/Dislab.Base/DbContexts/DapperContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Dislab.Base.DbContexts
+{
+    public class DapperContext : IDapperContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string? _configurationString;
+
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _configurationString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_configurationString);
+        }
+    }
+}
