14092c7e2fbf47de7772081c372a3669a5938e42
jahids-web <jahiduddin922@gmail.com>
2024-01-16 00:03:35 +0600

    Create New Repository Interface Class

---
+++ Dislab/Dislab.Base/Features/Questions/Domain/IAskQuestionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Dislab.Base.Features.Questions.Entities;
+
+namespace Dislab.Base.Features.Questions.Domain
+{
+    public interface IAskQuestionRepository
+    {
+        public AskQuestion Insert(AskQuestion question);
+        public void Update(AskQuestion question);
+        public long Delete(long id);
+        public IEnumerable<AskQuestion> GetAll();
+        public IEnumerable<AskQuestion> GetByQuestionId(long id);
+    }
+
+}
