7ee296ede3bfbbb453688705d6975bb2b860a717
jahids-web <jahiduddin922@gmail.com>
2024-01-16 00:01:22 +0600

    Create New InsertQuestionVM View model

---
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/InsertQuestionVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.ViewModels
+{
+    public class InsertQuestionVM
+    {
+        public Guid Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+    }
+}
