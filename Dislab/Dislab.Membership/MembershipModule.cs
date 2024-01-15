3ce6f29783e775773b5963efb37f976aba637410
jahids-web <jahiduddin922@gmail.com>
2024-01-15 23:54:49 +0600

    Create New MembershipModule Module

--- Dislab/Dislab.Membership/MembershipModule.cs
+++ Dislab/Dislab.Membership/MembershipModule.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Dislab.Membership
 {
-    public class MembershipModule
+    public class MembershipModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            base.Load(builder);
+        }
     }
 }
