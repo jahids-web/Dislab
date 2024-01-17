20f879bc79eb3147ed99ad827bd71c176bad2941
jahids-web <jahiduddin922@gmail.com>
2024-01-18 00:07:51 +0600

    Create Services Class

---
+++ Dislab/Dislab.Base/Services/QuestionService.cs
@@ -0,0 +1,42 @@
+﻿using Dislab.Base.Data;
+using Dislab.Base.Features.Questions.Entities;
+
+namespace Dislab.Base.Services
+{
+
+    public class QuestionService : IQuestionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public QuestionService(IUnitOfWork uniteOfWork)
+        {
+            _unitOfWork = uniteOfWork;
+        }
+
+        public AskQuestion Insert(AskQuestion question)
+        {
+            var result = _unitOfWork.AskQuestionRepository.Insert(question);
+            return result;
+        }
+
+        public long Delete(long id)
+        {
+            return _unitOfWork.AskQuestionRepository.Delete(id);
+        }
+
+        public IEnumerable<AskQuestion> GetAll()
+        {
+            return _unitOfWork.AskQuestionRepository.GetAll();
+        }
+
+        public IEnumerable<AskQuestion> GetByQuestionId(long id)
+        {
+            return _unitOfWork.AskQuestionRepository.GetByQuestionId(id);
+        }
+
+        public void Update(AskQuestion question)
+        {
+            _unitOfWork.AskQuestionRepository.Update(question);
+        }
+    }
+}
