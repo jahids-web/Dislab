454d746fa876726439fb70c55e8997dfd95b0b32
jahids-web <jahiduddin922@gmail.com>
2024-01-18 00:04:03 +0600

    Create InsertQuestionVm

--- Dislab/Dislab.Base/Features/Questions/ViewModels/InsertQuestionVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/InsertQuestionVM.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dislab.Base.Features.Questions.ViewModels
+﻿namespace Dislab.Base.Features.Questions.ViewModels
 {
     public class InsertQuestionVM
     {
