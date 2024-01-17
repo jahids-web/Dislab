b37cced00c56a66e3ddb5336fbcb52427507fa36
jahids-web <jahiduddin922@gmail.com>
2024-01-18 00:03:02 +0600

    Change Class Name

--- Dislab/Dislab.Base/Data/UnitOfWork.cs
+++ Dislab/Dislab.Base/Data/UnitOfWork.cs
@@ -5,16 +5,16 @@ namespace Dislab.Base.Data
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly IAskQuestionRepository _askQuestionRepository;
+        private readonly IQuestionRepository _askQuestionRepository;
         private readonly IDapperContext _dapperContext;
 
-        public UnitOfWork(IAskQuestionRepository askQuestionRepository, IDapperContext dapperContext)
+        public UnitOfWork(IQuestionRepository askQuestionRepository, IDapperContext dapperContext)
         {
             _askQuestionRepository = askQuestionRepository;
             _dapperContext = dapperContext;
         }
 
 
-        public IAskQuestionRepository AskQuestionRepository => _askQuestionRepository ?? new AskQuestionRepository(_dapperContext);
+        public IQuestionRepository AskQuestionRepository => _askQuestionRepository ?? new QuestionRepository(_dapperContext);
     }
 }
