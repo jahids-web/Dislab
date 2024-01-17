b1d96f2d2080476af387bddd61e6696b78605b91
jahids-web <jahiduddin922@gmail.com>
2024-01-18 00:05:52 +0600

    Create Services Class

---
+++ Dislab/Dislab.Base/Services/IQuestionService.cs
@@ -0,0 +1,18 @@
+﻿using Dislab.Base.Features.Questions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Services
+{
+    public interface IQuestionService
+    {
+        public AskQuestion Insert(AskQuestion question);
+        public void Update(AskQuestion question);
+        public long Delete(long id);
+        public IEnumerable<AskQuestion> GetAll();
+        public IEnumerable<AskQuestion> GetByQuestionId(long id);
+    }
+}
