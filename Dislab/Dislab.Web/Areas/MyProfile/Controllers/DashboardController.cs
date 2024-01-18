102545d544dc2010e647c7b22a9f094ffe158202
jahids-web <jahiduddin922@gmail.com>
2024-01-18 16:17:38 +0600

    Create New Controllers DashboardController

---
+++ Dislab/Dislab.Web/Areas/MyProfile/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dislab.Web.Areas.MyProfile.Controllers
+{
+    [Area("MyProfile")]
+    public class DashboardController : Controller
+    {
+
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
