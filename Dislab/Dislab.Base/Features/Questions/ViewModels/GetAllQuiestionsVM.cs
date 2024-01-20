fb47c27c369d388036118f5a7459cbd11d38b7c8
jahids-web <jahiduddin922@gmail.com>
2024-01-20 20:28:10 +0600

    Create New View Model

--- Dislab/Dislab.Base/Features/Questions/ViewModels/GetAllQuiestionsVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/GetAllQuiestionsVM.cs
@@ -1,9 +1,4 @@
 ﻿using Dislab.Base.Features.Questions.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dislab.Base.Features.Questions.ViewModels
 {
