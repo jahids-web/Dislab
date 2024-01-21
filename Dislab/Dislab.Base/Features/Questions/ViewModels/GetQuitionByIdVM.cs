65d8be1a58d882e170500d4cbe25eacccde42b83
jahids-web <jahiduddin922@gmail.com>
2024-01-22 00:25:00 +0600

    Create New ViewModel

---
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/GetQuitionByIdVM.cs
@@ -0,0 +1,9 @@
+﻿using Dislab.Base.Features.Questions.Entities;
+
+namespace Dislab.Base.Features.Questions.ViewModels
+{
+    public class GetQuitionByIdVM
+    {
+        public AskQuestion Question { get; set; }
+    }
+}
