ab5a368990bae7632e5e45696aa1a8ce94aa0a55
jahids-web <jahiduddin922@gmail.com>
2024-01-29 23:44:47 +0600

    Map Class

---
+++ Dislab/Dislab.Base/Features/Questions/Profiles/QuestionProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Dislab.Base.Features.Questions.DTOs;
+using Dislab.Base.Features.Questions.ViewModels;
+
+namespace Dislab.Base.Features.Questions.Profiles
+{
+    public class QuestionProfile : Profile
+    {
+        public QuestionProfile()
+        {
+            CreateMap<InsertQuestionVM, InsertQuestionDTO>();
+
+            //.ForMember(
+            //        dest => dest.QuestionTitle,
+            //        opt => opt.MapFrom(src => $"{src.QuestionTitle}")
+            //    )
+            //.ForMember(
+            //        dest => dest.QuestionTitle,
+            //        opt => opt.MapFrom(src => $"{src.QuestionTitle}")
+            //    );
+        }
+        
+    }
+}
