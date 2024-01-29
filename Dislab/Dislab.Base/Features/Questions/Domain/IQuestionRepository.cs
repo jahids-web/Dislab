57bdeef17c46abcb82aafdd7b15e9a003755c252
jahids-web <jahiduddin922@gmail.com>
2024-01-29 23:43:15 +0600

    Add DTO

--- Dislab/Dislab.Base/Features/Questions/Domain/IQuestionRepository.cs
+++ Dislab/Dislab.Base/Features/Questions/Domain/IQuestionRepository.cs
@@ -1,12 +1,12 @@
-﻿using Dislab.Base.Features.Questions.Entities;
-using Dislab.Base.Features.Questions.ViewModels;
+﻿using Dislab.Base.Features.Questions.DTOs;
+using Dislab.Base.Features.Questions.Entities;
 
 namespace Dislab.Base.Features.Questions.Domain
 {
     public interface IQuestionRepository
     {
-        public Task<bool>InsertAsync(InsertQuestionVM model);
-        public Task<string> UpdateAsync(AskQuestion question);
+        public Task<bool>InsertAsync(InsertQuestionDTO dto);
+        public Task<string> UpdateAsync(UpdateQuestionDTO dto);
         public Task<long> DeleteAsync(long id);
         public Task<IEnumerable<AskQuestion>> GetAllAsync();
         public Task<AskQuestion> GetByQuestionIdAsync(long id);
