ee209b2eefbc6d330a6fde5907f94fc0bf49ec1b
jahids-web <jahiduddin922@gmail.com>
2024-01-29 23:44:07 +0600

    Create DTO Class

---
+++ Dislab/Dislab.Base/Features/Questions/DTOs/UpdateQuestionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.DTOs
+{
+    public class UpdateQuestionDTO
+    {
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+    }
+}
