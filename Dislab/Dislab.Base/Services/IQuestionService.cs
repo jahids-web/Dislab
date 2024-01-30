1a91b0b7e86694993bbbf646564b2fa98fcb0f36
jahids-web <jahiduddin922@gmail.com>
2024-01-30 23:58:52 +0600

    Change Services Data

--- Dislab/Dislab.Base/Services/IQuestionService.cs
+++ Dislab/Dislab.Base/Services/IQuestionService.cs
@@ -1,4 +1,5 @@
-﻿using Dislab.Base.Features.Questions.Entities;
+﻿using Dislab.Base.Features.Questions.DTOs;
+using Dislab.Base.Features.Questions.Entities;
 using Dislab.Base.Features.Questions.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ namespace Dislab.Base.Services
     public interface IQuestionService
     {
         public Task<bool> InsertAsync(InsertQuestionVM model);
-        public Task<string> UpdateAsync(AskQuestion question);
+        public Task<string> UpdateAsync(UpdateQuestionVM model);
         public Task<long> DeleteAsync(long id);
         public Task<IEnumerable<AskQuestion>> GetAllAsync();
         public Task<AskQuestion> GetByQuestionIdAsync(long id);
