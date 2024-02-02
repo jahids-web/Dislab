94b8387d59b8e8afe79f8e3353ecd17b3d2154e8
jahids-web <jahiduddin922@gmail.com>
2024-02-02 18:53:33 +0600

    Create Ans Entities

---
+++ Dislab/Dislab.Base/Features/Answer/Entities/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.Entities
+{
+    public class Answer
+    {
+        public long QuestionId {  get; set; }
+        public string AnswerBody { get; set; }
+    }
+}
