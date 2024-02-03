e40d56d7940a90a169d24f4036d20621d7f4c93e
jahids-web <jahiduddin922@gmail.com>
2024-02-03 13:21:04 +0600

    Create Interface Repository Class

---
+++ Dislab/Dislab.Base/Features/Answer/Domain/IAnswerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dislab.Base.Features.Answer.ViewModel;
+
+namespace Dislab.Base.Features.Answer.Entities
+{
+    public interface IAnswerRepository
+    {
+        public Task<bool> InsertAsync(InsertAnswerVM model);
+        public Task<string> UpdateAsync(UpdateAnswerVM model);
+        public Task<long> DeleteAsync(long id);
+        public Task<IEnumerable<Answer>> GetAllAsync();
+        public Task<Answer> GetByQuestionIdAsync(long id);
+    }
+}
