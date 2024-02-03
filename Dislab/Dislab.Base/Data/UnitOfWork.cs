3165706433aa60983d3f62d7596e22434909a601
jahids-web <jahiduddin922@gmail.com>
2024-02-03 13:18:25 +0600

    Push Code

--- Dislab/Dislab.Base/Data/UnitOfWork.cs
+++ Dislab/Dislab.Base/Data/UnitOfWork.cs
@@ -1,4 +1,6 @@
 ﻿using Dislab.Base.DbContexts;
+using Dislab.Base.Features.Answer.Domain;
+using Dislab.Base.Features.Answer.Entities;
 using Dislab.Base.Features.Questions.Domain;
 
 namespace Dislab.Base.Data
@@ -6,15 +8,22 @@ namespace Dislab.Base.Data
     public class UnitOfWork : IUnitOfWork
     {
         private readonly IQuestionRepository _askQuestionRepository;
+        private readonly IAnswerRepository _answerRepository;
         private readonly IDapperContext _dapperContext;
 
-        public UnitOfWork(IQuestionRepository askQuestionRepository, IDapperContext dapperContext)
+        public UnitOfWork(
+            IQuestionRepository askQuestionRepository, 
+            IAnswerRepository answerRepository, 
+            IDapperContext dapperContext)
         {
             _askQuestionRepository = askQuestionRepository;
+            _answerRepository = answerRepository;
             _dapperContext = dapperContext;
         }
 
 
         public IQuestionRepository AskQuestionRepository => _askQuestionRepository ?? new QuestionRepository(_dapperContext);
+
+        public IAnswerRepository AnswerRepository => _answerRepository ?? new AnswerRepository(_dapperContext);
     }
 }
