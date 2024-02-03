6bd7aadea0d007037fdd959173a158a837e17600
jahids-web <jahiduddin922@gmail.com>
2024-02-03 13:18:06 +0600

    Push Some Code

--- Dislab/Dislab.Base/Data/IUnitOfWork.cs
+++ Dislab/Dislab.Base/Data/IUnitOfWork.cs
@@ -1,9 +1,13 @@
-﻿using Dislab.Base.Features.Questions.Domain;
+﻿using Dislab.Base.Features.Answer.Domain;
+using Dislab.Base.Features.Answer.Entities;
+using Dislab.Base.Features.Questions.Domain;
 
 namespace Dislab.Base.Data
 {
     public interface IUnitOfWork
     {
         public IQuestionRepository AskQuestionRepository { get; }
+
+        public IAnswerRepository AnswerRepository { get; }
     }
 }
