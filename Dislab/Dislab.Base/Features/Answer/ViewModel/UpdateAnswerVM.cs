fc3764e1a95a967225a7f191ce06382519fc21d1
jahids-web <jahiduddin922@gmail.com>
2024-02-03 13:21:51 +0600

    Create ViewModel Class

---
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/UpdateAnswerVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.ViewModel
+{
+    public class UpdateAnswerVM
+    {
+    }
+}
