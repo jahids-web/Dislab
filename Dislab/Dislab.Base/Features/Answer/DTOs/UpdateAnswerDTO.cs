542db9c6d0b41b40f374d926dd88764fb4af05c9
jahids-web <jahiduddin922@gmail.com>
2024-02-04 23:44:06 +0600

    Create UpdateAnswerDTO class

---
+++ Dislab/Dislab.Base/Features/Answer/DTOs/UpdateAnswerDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.DTOS
+{
+    public class UpdateAnswerDTO
+    {
+        public string? AnswerBody { get; set; }
+    }
+}
