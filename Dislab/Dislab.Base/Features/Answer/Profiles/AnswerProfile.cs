87aa8fdccadae2fb96f2b2e12ba60890133e0d51
jahids-web <jahiduddin922@gmail.com>
2024-02-04 23:44:29 +0600

    Create AnsProfile

---
+++ Dislab/Dislab.Base/Features/Answer/Profiles/AnswerProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Dislab.Base.Features.Answer.DTOS;
+using Dislab.Base.Features.Answer.ViewModel;
+using Dislab.Base.Features.Questions.DTOs;
+using Dislab.Base.Features.Questions.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.Profiles
+{
+    public class AnswerProfile : Profile
+    {
+        public AnswerProfile()
+        {
+            CreateMap<InsertAnswerVM, InsertAnswerDTO>()
+              .ForMember(dest => dest.AnswerBody, opt => opt.MapFrom(src => src.AnswerBody));
+
+            CreateMap<UpdateAnswerVM, UpdateAnswerDTO>()
+              .ForMember(dest => dest.AnswerBody, opt => opt.MapFrom(src => src.AnswerBody));
+        }
+     
+
+    }
+}
