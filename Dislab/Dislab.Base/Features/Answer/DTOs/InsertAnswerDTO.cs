4de98035172ff57f721d591288bdcf6f8de18084
jahids-web <jahiduddin922@gmail.com>
2024-02-04 23:43:41 +0600

    Create InsertAnswerDTO class

---
+++ Dislab/Dislab.Base/Features/Answer/DTOs/InsertAnswerDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.DTOS
+{
+    public class InsertAnswerDTO
+    {
+        public string? AnswerBody { get; set; }
+    }
+}
