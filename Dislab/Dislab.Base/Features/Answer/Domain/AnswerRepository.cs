38023e1b7159c35dd1ea1969aa9dac691daf2ecf
jahids-web <jahiduddin922@gmail.com>
2024-02-05 22:49:48 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/Domain/AnswerRepository.cs
+++ Dislab/Dislab.Base/Features/Answer/Domain/AnswerRepository.cs
@@ -20,102 +20,41 @@ namespace Dislab.Base.Features.Answer.Entities
         {
             try
             {
-                var sqlQuery = @"INSERT INTO Answer (QuestionId , AnswerBody) VALUES (@QuestionId, @AnswerBody)";
+                var sqlQuery = @"INSERT INTO Answer (QuestionId, AnswerBody) VALUES (@QuestionId, @AnswerBody)";
 
                 using var connection = _context.CreateConnection();
                 connection.Open();
                 var result = await connection.ExecuteAsync(sqlQuery, model);
-                if(result > 0)
+                if (result > 0)
                 {
                     return true;
                 }
                 return false;
             }
-            catch(Exception exception)
-            {
-                throw new InvalidOperationException(exception.Message, exception);
-            }
-        }
-
-        public async Task<string> UpdateAsync(UpdateAnswerDTO model)
-        {
-            try
-            {
-                var sqlQuery = @"UPDATE Answer SET AnswerBody = @AnswerBody WHERE Id = @Id";
-
-                using var connection = _context.CreateConnection();
-                connection.Open();
-                var result = await connection.QueryFirstOrDefaultAsync<long>(sqlQuery, model);
-                if (result > 0)
-                {
-                    return "Success";
-                }
-                else
-                {
-                    return "Not Success";
-                }
-            }
-            catch(Exception exception)
-            {
-                throw new InvalidOperationException(exception.Message, exception);
-            }
-        }
-
-        public async Task<IEnumerable<Answer>> GetAllAsync()
-        {
-            try
-            {
-                var sqlQuery = @"SELECT * FROM Answer";
-
-                using var connection = _context.CreateConnection();
-                connection.Open();
-                var result = await connection.QueryAsync<Answer>(sqlQuery);
-                return result;
-            }
             catch (Exception exception)
             {
                 throw new InvalidOperationException(exception.Message, exception);
             }
         }
 
-        public async Task<Answer> GetByIdAsync(long id)
+        public Task<long> DeleteAsync(long id)
         {
-            try
-            {
-                var sqlQuery = @"SELECT * FROM Answer WHERE Id = @id";
-
-                using var connection = _context.CreateConnection();
-                connection.Open();
-                var result = await connection.QueryFirstOrDefaultAsync<Answer>(sqlQuery, new { id });
-                return result;
-            }
-            catch (Exception exception)
-            {
-                throw new InvalidOperationException(exception.Message, exception);
-            }
+            throw new NotImplementedException();
         }
 
-        public async Task<long> DeleteAsync(long id)
+        public Task<IEnumerable<Answer>> GetAllAsync()
         {
-            try
-            {
-                var sqlQuery = @"DELETE FROM Answer WHERE Id = @id";
-
-                using var connection = _context.CreateConnection();
-                connection.Open();
-                var resutl = await connection.ExecuteAsync(sqlQuery, new { id });
-                return resutl;
-            }
-            catch (Exception exception)
-            {
-                throw new InvalidOperationException(exception.Message, exception);
-            }
+            throw new NotImplementedException();
         }
 
-       
+        public Task<Answer> GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
 
-       
-
-       
+        public Task<string> UpdateAsync(UpdateAnswerDTO model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
