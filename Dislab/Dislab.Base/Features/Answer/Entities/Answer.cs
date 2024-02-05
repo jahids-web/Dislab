bb1d4ff29eb5e48deb437a6e2d185f4e6479f7a1
jahids-web <jahiduddin922@gmail.com>
2024-02-05 22:50:13 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/Entities/Answer.cs
+++ Dislab/Dislab.Base/Features/Answer/Entities/Answer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Dislab.Base.Features.Questions.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +12,7 @@ namespace Dislab.Base.Features.Answer.Entities
     {
         public long QuestionId {  get; set; }
         public string? AnswerBody { get; set; }
+
+        //public AskQuestion? AskQuestion { get; set; }
     }
 }
