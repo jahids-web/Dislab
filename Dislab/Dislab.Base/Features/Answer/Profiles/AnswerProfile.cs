6fd434d9521038a00439264b8b6e43c601998467
jahids-web <jahiduddin922@gmail.com>
2024-02-05 22:50:29 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/Profiles/AnswerProfile.cs
+++ Dislab/Dislab.Base/Features/Answer/Profiles/AnswerProfile.cs
@@ -1,13 +1,6 @@
 ﻿using AutoMapper;
 using Dislab.Base.Features.Answer.DTOS;
 using Dislab.Base.Features.Answer.ViewModel;
-using Dislab.Base.Features.Questions.DTOs;
-using Dislab.Base.Features.Questions.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dislab.Base.Features.Answer.Profiles
 {
@@ -16,10 +9,12 @@ namespace Dislab.Base.Features.Answer.Profiles
         public AnswerProfile()
         {
             CreateMap<InsertAnswerVM, InsertAnswerDTO>()
+              .ForMember(dest => dest.QuestionId, opt => opt.MapFrom(src => src.QuestionId))
               .ForMember(dest => dest.AnswerBody, opt => opt.MapFrom(src => src.AnswerBody));
 
-            CreateMap<UpdateAnswerVM, UpdateAnswerDTO>()
-              .ForMember(dest => dest.AnswerBody, opt => opt.MapFrom(src => src.AnswerBody));
+            //CreateMap<UpdateAnswerVM, UpdateAnswerDTO>()
+            //  .ForMember(dest => dest.QuestionId, opt => opt.MapFrom(src => src.QuestionId))
+            //  .ForMember(dest => dest.AnswerBody, opt => opt.MapFrom(src => src.AnswerBody));
         }
      
 
