d33d358411e87e464c58d790bb45f39b5d7498f5
jahids-web <jahiduddin922@gmail.com>
2024-02-07 23:18:12 +0600

    Create Ans controller

---
+++ Dislab/Dislab.Web/Areas/MyProfile/Controllers/AnswerController.cs
@@ -0,0 +1,49 @@
+﻿using Dislab.Base.Features.Answer.ViewModel;
+using Dislab.Base.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dislab.Web.Areas.MyProfile.Controllers
+{
+    [Area("MyProfile")]
+    public class AnswerController : Controller
+    {
+        private readonly IAnswerService _answerService;
+
+        public AnswerController(IAnswerService answerService)
+        {
+            _answerService = answerService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert(InsertAnswerVM model)
+        {
+            //model.QuestionId = 0;
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    var result = await _answerService.InsertAsync(model);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+
+
+
+    }
+}
