fabf243e3d200f988a367becc91d1dbd395fea8b
jahids-web <jahiduddin922@gmail.com>
2024-02-10 23:08:39 +0600

    Create QuestionDetailsDTO Class

---
+++ Dislab/Dislab.Base/Features/Questions/DTOs/QuestionDetailsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.DTOs
+{
+    public class QuestionDetailsDTO
+    {
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+
+        public long QuestionId { get; set; }
+        public string? AnswerBody { get; set; }
+    }
+}
