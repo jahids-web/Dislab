0eb9de4b9086e3fdddcd40e1ef5f7e754facbf28
jahids-web <jahiduddin922@gmail.com>
2024-02-10 23:13:26 +0600

    Create QuestionDetailsVM Class

---
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/QuestionDetailsVM.cs
@@ -0,0 +1,29 @@
+﻿using Dislab.Base.Features.Answer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.ViewModels
+{
+    public class QuestionDetailsVM
+    {
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+
+        public long QuestionId {  get; set; }
+        public string? AnswerBody {  get; set; }
+
+        public InsertAnswerVM GetAnswerVM()
+        {
+            var model = new InsertAnswerVM
+            {
+                QuestionId = QuestionId,
+                AnswerBody = AnswerBody
+            };
+            return model;
+        }
+    }
+}
