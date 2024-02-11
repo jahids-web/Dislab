16a318839e02d3c4beff2a13675c039636b38b0d
jahids-web <jahiduddin922@gmail.com>
2024-02-11 21:24:43 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/Domain/IAnswerRepository.cs
+++ Dislab/Dislab.Base/Features/Answer/Domain/IAnswerRepository.cs
@@ -1,4 +1,5 @@
-﻿using Dislab.Base.Features.Answer.DTOS;
+﻿using Dislab.Base.Features.Answer.DTOs;
+using Dislab.Base.Features.Answer.DTOS;
 using Dislab.Base.Features.Answer.ViewModel;
 
 namespace Dislab.Base.Features.Answer.Entities
@@ -6,9 +7,9 @@ namespace Dislab.Base.Features.Answer.Entities
     public interface IAnswerRepository
     {
         public Task<bool> InsertAsync(InsertAnswerDTO model);
-        public Task<string> UpdateAsync(UpdateAnswerDTO model);
+        public Task<string> UpdateAsync(GetAnswerByIdDTO model);
         public Task<long> DeleteAsync(long id);
         public Task<IEnumerable<Answer>> GetAllAsync();
-        public Task<Answer> GetByIdAsync(long id);
+        public Task<GetAnswerByIdDTO> GetAnswerByIdAsync(long id);
     }
 }
