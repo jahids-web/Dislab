eb1d29095ddaca778be8b8b320ee17d3aed8e27f
jahids-web <jahiduddin922@gmail.com>
2024-02-11 21:28:55 +0600

    Delete VM

--- Dislab/Dislab.Base/Features/Questions/ViewModels/GetQuitionByIdVM.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dislab.Base.Features.Questions.Entities;
-
-namespace Dislab.Base.Features.Questions.ViewModels
-{
-    public class GetQuitionByIdVM
-    {
-        public Question? Question { get; set; }
-    }
-}
