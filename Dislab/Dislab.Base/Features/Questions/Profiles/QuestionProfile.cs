51e3f46661ed687e08a70a05871716ecc6ed2f79
jahids-web <jahiduddin922@gmail.com>
2024-02-11 22:03:35 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Questions/Profiles/QuestionProfile.cs
+++ Dislab/Dislab.Base/Features/Questions/Profiles/QuestionProfile.cs
@@ -17,10 +17,6 @@ namespace Dislab.Base.Features.Questions.Profiles
                .ForMember(dest => dest.QuestionTitle, opt => opt.MapFrom(src => src.QuestionTitle))
                .ForMember(dest => dest.QuestionBody, opt => opt.MapFrom(src => src.QuestionBody));
 
-            
-            CreateMap<PostQuestionDetailesVM, PostQuestionDetailesDTO>()
-          .ForMember(dest => dest.Question, opt => opt.MapFrom(src => src.Question));
-
             CreateMap<QuestionDetailsVM, QuestionDetailsDTO>().ReverseMap();
 
             CreateMap<GetAllQuiestionsVM, GetAllQuiestionsDTO>().ReverseMap();
