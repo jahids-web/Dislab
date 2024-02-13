f4fd57f5b1fa43af077b491aa34c316b0dd9c7d6
jahids-web <jahiduddin922@gmail.com>
2024-02-13 23:23:10 +0600

    Create new vm class

---
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/AnswerVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.ViewModel
+{
+    public class AnswerVM
+    {
+        public long id {  get; set; }
+        public string? AnswerBody { get; set; }
+    }
+}
