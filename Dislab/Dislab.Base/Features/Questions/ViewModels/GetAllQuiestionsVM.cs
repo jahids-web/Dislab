74983d48d67dfac579d407acbdbae1b2d3e1bb1e
jahids-web <jahiduddin922@gmail.com>
2024-02-14 23:28:24 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Questions/ViewModels/GetAllQuiestionsVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/GetAllQuiestionsVM.cs
@@ -4,6 +4,8 @@ namespace Dislab.Base.Features.Questions.ViewModels
 {
     public class GetAllQuiestionsVM
     {
-        public IEnumerable<Question>? Questions { get; set; }
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
     }
 }
--- Dislab/Dislab.Base/Features/Questions/ViewModels/InsertQuestionVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/InsertQuestionVM.cs
@@ -5,9 +5,14 @@ namespace Dislab.Base.Features.Questions.ViewModels
     public class InsertQuestionVM
     {
         [Required]
+        [StringLength(150, ErrorMessage = "Question Title can't be more than 150 characters.")]
         public string? QuestionTitle { get; set; }
 
         [Required]
+        [StringLength(2000, ErrorMessage = "Question Description can't be more than 2000 characters.")]
         public string? QuestionBody { get; set; }
+
     }
+ 
+
 }
