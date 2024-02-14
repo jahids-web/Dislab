c1bb52ca4b778159bbf380e5b07757a8954b6251
jahids-web <jahiduddin922@gmail.com>
2024-02-14 23:27:30 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/ViewModel/GetAllAnswerVM.cs
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/GetAllAnswerVM.cs
@@ -1,6 +1,7 @@
 ﻿using Dislab.Base.Features.Questions.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Dislab.Base.Features.Answer.ViewModel
 {
     public class GetAllAnswerVM
     {
-        public IEnumerable<GetAllAnswerVM>? GetAllAnswerVMs {  get; set; }
+        public long Id { get; set; }
+        public long QuestionId { get; set; }
+        public string? AnswerBody { get; set; }
     }
 }
