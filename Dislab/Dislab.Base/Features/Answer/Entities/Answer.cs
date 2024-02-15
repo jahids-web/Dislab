ccc95ffc02aa8cf388cb3152ced6565311de23e1
jahids-web <jahiduddin922@gmail.com>
2024-02-16 00:06:40 +0600

    Push Code

--- Dislab/Dislab.Base/Features/Answer/Entities/Answer.cs
+++ Dislab/Dislab.Base/Features/Answer/Entities/Answer.cs
@@ -10,11 +10,8 @@ namespace Dislab.Base.Features.Answer.Entities
 {
     public class Answer
     {
-      
         public long Id { get; set; }
         public long QuestionId {  get; set; }
         public string? AnswerBody { get; set; }
-
-
     }
 }
