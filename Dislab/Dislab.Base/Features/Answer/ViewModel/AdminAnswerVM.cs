493a43a4ee9349bd79d874097e4b944cc7e7ddc7
jahids-web <jahiduddin922@gmail.com>
2024-02-24 22:58:43 +0600

    Create AdminAnswerVM class

---
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/AdminAnswerVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.ViewModel
+{
+    public class AdminAnswerVM
+    {
+        public long Id { get; set; }
+        public string? AnswerBody { get; set; }
+    }
+}
