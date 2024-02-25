1b3bb947d2097e5c6e367282c7a37cb8e7626d1b
jahids-web <jahiduddin922@gmail.com>
2024-02-25 17:50:20 +0600

    Create AdminQuestionDTO Class

---
+++ Dislab/Dislab.Base/Features/Questions/DTOs/AdminQuestionDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.DTOs
+{
+    public class AdminQuestionDTO
+    {
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+    }
+}
