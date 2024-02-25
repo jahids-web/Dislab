ebf9e7f0b4ab72d82e238c9dfe554ca85e81ce20
jahids-web <jahiduddin922@gmail.com>
2024-02-25 17:50:42 +0600

    Create AdminQuestionVM Class

---
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/AdminQuestionVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Questions.ViewModels
+{
+    public class AdminQuestionVM
+    {
+        public long Id { get; set; }
+        public string? QuestionTitle { get; set; }
+        public string? QuestionBody { get; set; }
+    }
+}
