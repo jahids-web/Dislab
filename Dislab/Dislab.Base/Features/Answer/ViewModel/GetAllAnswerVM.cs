9a49bfb15f2582bdad5de39c46e8baf204709e4c
jahids-web <jahiduddin922@gmail.com>
2024-02-29 01:05:23 +0600

    Add New Field

--- Dislab/Dislab.Base/Features/Answer/ViewModel/GetAllAnswerVM.cs
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/GetAllAnswerVM.cs
@@ -1,17 +1,10 @@
-﻿using Dislab.Base.Features.Questions.Entities;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dislab.Base.Features.Answer.ViewModel
+﻿namespace Dislab.Base.Features.Answer.ViewModel
 {
     public class GetAllAnswerVM
     {
         public long Id { get; set; }
         public long QuestionId { get; set; }
+        public string QuestionTitle { get; set; }
         public string? AnswerBody { get; set; }
     }
 }
