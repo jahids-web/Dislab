6c3bfa78be02c4df71f847c13f44d8b7f3e96051
jahids-web <jahiduddin922@gmail.com>
2024-02-29 01:04:04 +0600

    Add New Field

--- Dislab/Dislab.Base/Features/Answer/DTOs/GetAllAnswerDTO.cs
+++ Dislab/Dislab.Base/Features/Answer/DTOs/GetAllAnswerDTO.cs
@@ -6,6 +6,7 @@ namespace Dislab.Base.Features.Answer.DTOs
     {
         public long Id { get; set; }
         public long QuestionId { get; set; }
+        public string QuestionTitle { get; set; }
         public string? AnswerBody { get; set; }
     }
 }
