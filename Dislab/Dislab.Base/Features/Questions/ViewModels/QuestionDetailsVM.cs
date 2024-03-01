6f517cd2da98001f37064b00121ecee7e804d6b9
jahids-web <jahiduddin922@gmail.com>
2024-03-01 23:14:32 +0600

    Comment Some Code

--- Dislab/Dislab.Base/Features/Questions/ViewModels/QuestionDetailsVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/QuestionDetailsVM.cs
@@ -1,6 +1,7 @@
 ﻿using Dislab.Base.Features.Answer.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,20 @@ namespace Dislab.Base.Features.Questions.ViewModels
     public class QuestionDetailsVM
     {
         public long Id { get; set; }
+
+        //[Required]
+        //[StringLength(150, ErrorMessage = "Question Title can't be more than 150 characters.")]
         public string? QuestionTitle { get; set; }
+
+        //[Required]
+        //[StringLength(2000, ErrorMessage = "Question Description can't be more than 2000 characters.")]
         public string? QuestionBody { get; set; }
 
         public long QuestionId { get; set; }
         public long AnswerId { get; set; }
+
+        //[Required]
+        //[StringLength(3000, ErrorMessage = "Answer Body can't be more than 3000 characters.")]
         public string? AnswerBody {  get; set; }
 
         public List<AnswerVM>? Answers { get; set; } //Ekane sob Answer list ante hobe loop e kaj korar juno
