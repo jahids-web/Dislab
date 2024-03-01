d6e8675d3597663d4b3ddda8a3e3ae48aeddcb59
jahids-web <jahiduddin922@gmail.com>
2024-03-01 23:14:23 +0600

    Comment Some Code

--- Dislab/Dislab.Base/Features/Questions/ViewModels/UpdateQuestionVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/UpdateQuestionVM.cs
@@ -8,12 +8,12 @@ namespace Dislab.Base.Features.Questions.ViewModels
         public long Id { get; set; }
         //public long QuestionId { get; set; }
 
-        [Required]
-        [StringLength(150, ErrorMessage = "Question Title can't be more than 150 characters.")]
+        //[Required]
+        //[StringLength(150, ErrorMessage = "Question Title can't be more than 150 characters.")]
         public string? QuestionTitle { get; set; }
 
-        [Required]
-        [StringLength(2000, ErrorMessage = "Question Description can't be more than 2000 characters.")]
+        //[Required]
+        //[StringLength(2000, ErrorMessage = "Question Description can't be more than 2000 characters.")]
         public string? QuestionBody { get; set; }
     }
 }
