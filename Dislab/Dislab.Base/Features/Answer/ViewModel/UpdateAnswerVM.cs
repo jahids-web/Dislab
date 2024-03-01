13324e1cc9997d5e05e2ea8dd7fc3bd08d9aa6ee
jahids-web <jahiduddin922@gmail.com>
2024-03-01 23:13:22 +0600

    Comment Some Code

--- Dislab/Dislab.Base/Features/Answer/ViewModel/UpdateAnswerVM.cs
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/UpdateAnswerVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace Dislab.Base.Features.Answer.ViewModel
     public class UpdateAnswerVM
     {
         public long Id { get; set; }
+
+        //[Required]
+        //[StringLength(3000, ErrorMessage = "Answer Body can't be more than 3000 characters.")]
         public string? AnswerBody { get; set; }
     }
 }
