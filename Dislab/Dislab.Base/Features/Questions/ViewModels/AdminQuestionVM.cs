c12cf8731045cb90f1b7d25a1e54f805d4f48333
jahids-web <jahiduddin922@gmail.com>
2024-03-01 23:14:00 +0600

    Comment Some Code

--- Dislab/Dislab.Base/Features/Questions/ViewModels/AdminQuestionVM.cs
+++ Dislab/Dislab.Base/Features/Questions/ViewModels/AdminQuestionVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace Dislab.Base.Features.Questions.ViewModels
     public class AdminQuestionVM
     {
         public long Id { get; set; }
+
+        //[Required]
+        //[StringLength(150, ErrorMessage = "Question Title can't be more than 150 characters.")]
         public string? QuestionTitle { get; set; }
+
+        //[Required]
+        //[StringLength(2000, ErrorMessage = "Question Description can't be more than 2000 characters.")]
         public string? QuestionBody { get; set; }
     }
 }
