3c52cf2b87d4c6547445db1ab7f6230aaec5364f
jahids-web <jahiduddin922@gmail.com>
2024-03-06 00:18:43 +0600

    Remove some Field

--- Dislab/Dislab.Base/Features/Questions/Entities/Question.cs
+++ Dislab/Dislab.Base/Features/Questions/Entities/Question.cs
@@ -4,12 +4,9 @@ namespace Dislab.Base.Features.Questions.Entities
 {
     public class Question
     {
-      
         public long Id { get; set; }
         public string? QuestionTitle { get; set; }
         public string? QuestionBody { get; set; }
-        public int UpVotes { get; set; }
-        public int DownVotes { get; set; }
 
     }
 }
