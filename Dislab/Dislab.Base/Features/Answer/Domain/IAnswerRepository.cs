362dda210d63dd292fd97f579d376b5df76a25bc
jahids-web <jahiduddin922@gmail.com>
2024-03-05 23:29:59 +0600

    Remove Some Using

--- Dislab/Dislab.Base/Features/Answer/Domain/IAnswerRepository.cs
+++ Dislab/Dislab.Base/Features/Answer/Domain/IAnswerRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dislab.Base.Features.Answer.DTOs;
 using Dislab.Base.Features.Answer.DTOS;
 using Dislab.Base.Features.Answer.ViewModel;
+using Dislab.Base.Features.Questions.DTOs;
 
 namespace Dislab.Base.Features.Answer.Entities
 {
@@ -11,7 +12,6 @@ namespace Dislab.Base.Features.Answer.Entities
         public Task<long> DeleteFEAsync(long id);
         public Task<IEnumerable<GetAllAnswerDTO>> GetAllAnswerAsync(long id);
         public Task<GetAnswerByIdDTO> GetAnswerByIdFEAsync(long id);
-
         //DashBoard
         public Task<AdminAnswerDTO> GetAnswerByIdAsync(long id);
         public Task<string> UpdateAsync(AdminAnswerDTO model);
