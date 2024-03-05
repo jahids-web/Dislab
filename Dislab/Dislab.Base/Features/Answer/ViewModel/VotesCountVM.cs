6453bf7a9ed9e2b43d763a854c97fda98cc96e65
jahids-web <jahiduddin922@gmail.com>
2024-03-06 00:22:32 +0600

    Create New Vm

---
+++ Dislab/Dislab.Base/Features/Answer/ViewModel/VotesCountVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.ViewModel
+{
+    public class VotesCountVM
+    {
+        public long Id { get; set; }
+        public long VoteCount { get; set; }
+       
+    }
+}
