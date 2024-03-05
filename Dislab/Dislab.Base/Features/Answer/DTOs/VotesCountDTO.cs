c8e45a6d409e28cb7bba4613885cc7825df4a6cd
jahids-web <jahiduddin922@gmail.com>
2024-03-06 00:22:18 +0600

    Create New DTO

---
+++ Dislab/Dislab.Base/Features/Answer/DTOs/VotesCountDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dislab.Base.Features.Answer.DTOs
+{
+    public class VotesCountDTO
+    {
+        public long Id { get; set; }
+        public long VoteCount { get; set; }
+    }
+}
